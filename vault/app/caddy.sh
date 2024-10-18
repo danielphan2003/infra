67664337bf7d95ede080bec3f2d64128f7cdbce0
Daniel Phan <danielphan.2003@gmail.com>
2024-10-18 17:31:58 +0700

    fix(vault): unset unnecessary env var

--- vault/app/caddy.sh
+++ vault/app/caddy.sh
@@ -1,4 +1,28 @@
 #! /usr/bin/with-contenv bash
+# Unset Cloudflare environment variables
+unset CF_ACCOUNT_ID
+unset CF_API_TOKEN
+unset CF_TUNNEL_NAME
+unset CF_TUNNEL_PASSWORD
+unset CF_USER
+unset CF_ZONE_ID
+
+# Unset Vaultwarden environment variables
+unset DATABASE_ENDPOINT
+unset DATABASE_OPTION
+unset DATABASE_PASSWORD
+unset DATABASE_URL
+unset DATABASE_USERNAME
+unset PUSH_INSTALLATION_ID
+unset PUSH_INSTALLATION_KEY
+unset SMTP_PASSWORD
+unset SMTP_USERNAME
+
+# Unset Tailscale environment variables
+unset TAILSCALE_AUTHKEY
+unset TS_AUTHKEY_VAULT
+unset TS_VAULT_ADMIN_TAILNET
+unset TS_VAULT_ADMIN_USER
 
 mkdir -p /data/caddy/data /data/caddy/config
 
--- vault/app/vaultwarden.sh
+++ vault/app/vaultwarden.sh
@@ -1,4 +1,17 @@
 #! /usr/bin/with-contenv bash
+# Unset Cloudflare environment variables
+unset CF_ACCOUNT_ID
+unset CF_API_TOKEN
+unset CF_TUNNEL_NAME
+unset CF_TUNNEL_PASSWORD
+unset CF_USER
+unset CF_ZONE_ID
+
+# Unset Tailscale environment variables
+unset TAILSCALE_AUTHKEY
+unset TS_AUTHKEY_VAULT
+unset TS_VAULT_ADMIN_TAILNET
+unset TS_VAULT_ADMIN_USER
 
 mkdir -p /data/vault
 
